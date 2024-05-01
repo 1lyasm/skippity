c6eb3c45d9c7b08fee9ecc73ea7c8e3a56060e28
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-05-01 21:58:23 +0300

    Add gameEnds

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 
 #define N_SKIPPER 5
 
@@ -88,12 +88,12 @@ static int findMin(int *arr, int n) {
 }
 
 static void printArr(int *counts, int n) {
-    int i;
-    printf("\n");
-    for (i = 0; i < n; ++i) {
-        printf("%d ", counts[i]);
-    }
-    printf("\n");
+  int i;
+  printf("\n");
+  for (i = 0; i < n; ++i) {
+    printf("%d ", counts[i]);
+  }
+  printf("\n");
 }
 
 static int compScore(int player, int *counts0, int *counts1) {
@@ -114,19 +114,23 @@ static int compScore(int player, int *counts0, int *counts1) {
 }
 
 static void save(char **b, size_t n, int player) {
-    char *buf = calloc((size_t)(n * n + 2), sizeof(char));
-    size_t i, j, k = 0;
-    FILE *outf;
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            buf[k++] = b[i][j];
-        }
+  char *buf = calloc((size_t)(n * n + 2), sizeof(char));
+  size_t i, j, k = 0;
+  FILE *outf;
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      buf[k++] = b[i][j];
     }
-    buf[k++] = (char) player + '0';
-    printf("\n");
-    outf = fopen("skippity.txt", "w");
-    fprintf(outf, "%s", buf);
-    free(buf);
+  }
+  buf[k++] = (char)player + '0';
+  printf("\n");
+  outf = fopen("skippity.txt", "w");
+  fprintf(outf, "%s", buf);
+  free(buf);
+}
+
+static int gameEnds(char **b, size_t n) {
+    return 0;
 }
 
 static void playHuman(char **b, size_t n, char *colors, int player) {
@@ -165,13 +169,12 @@ static void playHuman(char **b, size_t n, char *colors, int player) {
         switchP(&player, &nUndo, &nRedo, &wantsRedo, &hPos);
       }
     } else {
-        printf("\nDo you want to save and exit ('y': yes)? ");
-        scanf(" %c", &input);
-        if (input == 'y') {
-            save(b, n, player);
-            exit(EXIT_SUCCESS);
-        }
-
+      printf("\nDo you want to save and exit ('y': yes)? ");
+      scanf(" %c", &input);
+      if (input == 'y') {
+        save(b, n, player);
+        exit(EXIT_SUCCESS);
+      }
       printf("\nPlayer %c, enter your move (x0, y0, x1, y1): ", player + '0');
       scanf(" %d %d %d %d", &p.x0, &p.y0, &p.x1, &p.y1);
       /* printf("\nMove: (%d, %d), (%d, %d)\n", p.x0, p.y0, p.x1, p.y1); */
@@ -182,29 +185,33 @@ static void playHuman(char **b, size_t n, char *colors, int player) {
       printBoard(b, n);
       score = compScore(player, counts0, counts1);
       printf("\nScore of player %d: %d\n", player, score);
-      if (nUndo <= 0) {
-        printf("\nUndo ('y': yes, 'n': no)? ");
-        scanf(" %c", &input);
-        undoes = input == 'y';
-        if (undoes && hPos <= -1) {
-          printf("\nCan not undo\n");
-          printBoard(b, n);
+      if (gameEnds(b, n)) {
+        ended = 1;
+      } else {
+        if (nUndo <= 0) {
+          printf("\nUndo ('y': yes, 'n': no)? ");
+          scanf(" %c", &input);
+          undoes = input == 'y';
+          if (undoes && hPos <= -1) {
+            printf("\nCan not undo\n");
+            printBoard(b, n);
+          } else {
+            canUndo = 1;
+          }
+          printf("\nCan undo: %d\n", canUndo);
         } else {
-          canUndo = 1;
+          undoes = 0;
         }
-        printf("\nCan undo: %d\n", canUndo);
-      } else {
-        undoes = 0;
-      }
-      if (!(undoes && nUndo == 0) || !canUndo) {
-        switchP(&player, &nUndo, &nRedo, &wantsRedo, &hPos);
-      } else {
-        undo(b, &p, &h);
-        if (undoes) {
-          --hPos;
-          nUndo += 1;
+        if (!(undoes && nUndo == 0) || !canUndo) {
+          switchP(&player, &nUndo, &nRedo, &wantsRedo, &hPos);
+        } else {
+          undo(b, &p, &h);
+          if (undoes) {
+            --hPos;
+            nUndo += 1;
+          }
+          printBoard(b, n);
         }
-        printBoard(b, n);
       }
     }
   }
@@ -224,42 +231,42 @@ int main() {
   printf("Do you want to continue the previous game ('y': yes)? ");
   scanf(" %c", &input);
   if (input == 'y') {
-      FILE *inf = fopen("skippity.txt", "r");
-      char *buf = malloc(1024 * sizeof(char));
-      size_t k;
-      fgets(buf, 1024, inf);
-      fclose(inf);
-      printf("\nBuf: %s\n", buf);
-      k = strlen(buf);
-      n = (size_t)(sqrt((double)(k - 1)));
-      printf("\nN: %lu\n", n);
-      b = malloc(n * sizeof(char *));
-      for (i = 0; i < n; ++i) {
-          b[i] = malloc(n * sizeof(char));
-      }
-      for (i = 0; i < k - 1; ++i) {
-          /* printf("i / n: %d\n", i / n); */
-          b[i / n][i % n] = buf[i];
-      }
-      player = buf[k - 1] - '0';
-      free(buf);
+    FILE *inf = fopen("skippity.txt", "r");
+    char *buf = malloc(1024 * sizeof(char));
+    size_t k;
+    fgets(buf, 1024, inf);
+    fclose(inf);
+    printf("\nBuf: %s\n", buf);
+    k = strlen(buf);
+    n = (size_t)(sqrt((double)(k - 1)));
+    printf("\nN: %lu\n", n);
+    b = malloc(n * sizeof(char *));
+    for (i = 0; i < n; ++i) {
+      b[i] = malloc(n * sizeof(char));
+    }
+    for (i = 0; i < k - 1; ++i) {
+      /* printf("i / n: %d\n", i / n); */
+      b[i / n][i % n] = buf[i];
+    }
+    player = buf[k - 1] - '0';
+    free(buf);
   } else {
-      printf("\nEnter board size: ");
-      scanf(" %lu", &n);
-      b = malloc(n * sizeof(char *));
-      for (i = 0; i < n; ++i) {
-        b[i] = malloc(n * sizeof(char));
+    printf("\nEnter board size: ");
+    scanf(" %lu", &n);
+    b = malloc(n * sizeof(char *));
+    for (i = 0; i < n; ++i) {
+      b[i] = malloc(n * sizeof(char));
+    }
+    for (i = 0; i < n; ++i) {
+      for (j = 0; j < n; ++j) {
+        b[i][j] = colors[rand() % N_SKIPPER + 1];
       }
-      for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-          b[i][j] = colors[rand() % N_SKIPPER + 1];
-        }
+    }
+    for (i = n / 2 - 1; i <= n / 2; ++i) {
+      for (j = n / 2 - 1; j <= n / 2; ++j) {
+        b[i][j] = colors[0];
       }
-      for (i = n / 2 - 1; i <= n / 2; ++i) {
-        for (j = n / 2 - 1; j <= n / 2; ++j) {
-          b[i][j] = colors[0];
-        }
-      }
+    }
   }
   printf("\nBoard: \n");
   printBoard(b, n);
